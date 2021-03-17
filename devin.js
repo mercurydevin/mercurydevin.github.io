8dc64eafb5d7ba507ff90f94c8ef5c69130e24a0
mercurydevin <80853765+mercurydevin@users.noreply.github.com>
2021-03-17 16:23:46 -0400

    Create devin.js

---
+++ devin.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', scrollEvent);
+
+function scrollEvent(evt) {
+	const target = document.querySelectorAll('.scroll');
+	
+	for (var i = 0; i < target.length; i ++) {
+		var pos = window.pageYOffset * target[i].dataset.rate;
+		
+		if (target[i].dataset.direction == 'v') {
+			target[i].style.transform = "translate3d(0px," + pos + "px, 0px)";
+		}
+		else {
+			target[i].style.transform = "translate3d("+ pos +"px,"+window.pageYOffset+"px,0px)";
+		}
+	}
+}
